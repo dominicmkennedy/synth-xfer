2815bac6399837b89d25c327564b842e952be948
danielbonkowsky <danielbonkowsky@gmail.com>
2026-02-26 14:50:59 -0800

    Files for cli tools to run

---
+++ stitch/llmain.py
@@ -0,0 +1,8 @@
+import sys
+
+def main():
+    print("Running LLM library learning")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ stitch/main.py
@@ -0,0 +1,8 @@
+import sys
+
+def main():
+    print("Running Stitch library learning")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
