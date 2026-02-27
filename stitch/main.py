b3f88e91a0fa59017e8533ff0930a10e579ebdcc
danielbonkowsky <danielbonkowsky@gmail.com>
2026-02-27 07:23:54 -0800

    Set up cli with mlir to lam to mlir conversion

--- stitch/main.py
+++ stitch/main.py
@@ -1,5 +1,9 @@
 import sys
 import argparse
+from stitch_core import compress
+from .mlir_to_lam import mlir_file_to_lam
+from .lam_to_mlir import lamstr_to_mlir
+from pathlib import Path
 
 def main():
     """ Perform library learning with Stitch"""
@@ -11,7 +15,7 @@ def main():
         help="one or more MLIR files to learn from (e.g., mlir/Add.mlir mlir/Sub.mlir)"
     )
     parser.add_argument(
-        "-o", "--output", default="outputs/learned/llm/", help="output directory"
+        "-o", "--output", default="stdout", help="output directory"
     )
     parser.add_argument(
         "--iterations",
@@ -27,6 +31,22 @@ def main():
     )
 
     args = parser.parse_args()
+
+    programs = []
+    for file_path in args.library_files:
+        programs += mlir_file_to_lam(Path(file_path))
+    
+    print(f"Read {len(programs)} functions")
+
+    res = compress(programs, iterations=args.iterations, max_arity=args.max_arity)
+    print("Library learning complete")
+
+    if (args.output != 'stdout'):
+        with open(args.output, 'w') as file:
+            file.write(lamstr_to_mlir(''.join(f'{a}\n' for a in res.abstractions)))
+        print(f"Wrote to file {args.output}")
+    else:
+        print(lamstr_to_mlir(''.join(f'{a}\n' for a in res.abstractions)))
 
 if __name__ == "__main__":
     sys.exit(main())
