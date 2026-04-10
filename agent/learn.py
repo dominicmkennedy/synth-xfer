22894fd8773445ea692f06ef7428c9e5f93cddd0
danielbonkowsky <danielbonkowsky@gmail.com>
2026-04-10 11:42:02 -0700

    Check solution_iters and solution_text for solutions

--- agent/learn.py
+++ agent/learn.py
@@ -36,12 +36,12 @@ def _run_agent_learn(
     @function_tool
     def get_corpus_functions() -> str:
         """Return the corpus MLIR programs to learn library funcs from"""
-        text = ""
+        corpus = set()
         for result in synthesis_results:
+            corpus.add(result.solution_text)
             for soln in result.solution_iters:
-                text += f"{soln}\n"
-
-        return text
+                corpus.add(soln)
+        return "\n".join([str(s) for s in corpus])
 
     @function_tool
     def get_available_primitives() -> str:
